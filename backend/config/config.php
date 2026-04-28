b1c8debc576de86061fa7965b2a3cc82b70826bb
kinrishop226 <kiendrebeogoabdoulrahim63@gmail.com>
2026-04-28 13:14:45 +0000

    Update config.php

--- backend/config/config.php
+++ backend/config/config.php
@@ -3,7 +3,16 @@
 //  EduSchedule Pro — Configuration Générale
 //  Auteur : Bechir
 // ============================================================
+// CORS
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 // URL de base de l'API
 define('BASE_URL', 'http://localhost/EduSchedule-Pro/backend');
 
