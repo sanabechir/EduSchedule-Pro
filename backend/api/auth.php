57fe847e7c723a32594d803586268b316959545d
sanabechir <sanabechir75@gmail.com>
2026-05-01 11:27:51 +0000

    Update auth.php

--- backend/api/auth.php
+++ backend/api/auth.php
@@ -8,6 +8,7 @@ header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('X-Auth-Version: CLEAN_AUTH_V3');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
